29e7e7092ffbbc231592233483f3aaa46fd38bb9
someone-1 <iam.nageshbs@gmail.com>
2017-01-29 11:24:09 +0530

    optimized

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,41 @@
 app = angular.module("app",[]);
 app.controller('main', ['$scope', function($scope){
 
-	$scope.name="here";
-
-	$scope.date = new Date('2/1/2016');
+	$scope.date = new Date('12/1/2017');
 
 }])
-
+ 
 
 app.directive('cal', function(){
 
 	var directive = {};
-
 	directive.restrict = 'E';
-
 	directive.templateUrl = "/assets/template.html";
-
 	directive.scope = {
 		date : "=date"
 	};
 
 	directive.controller = function($scope){
-		$scope.name = 'the name';
-		var d = $scope.date;
-		var m = (d.getMonth()+2)%12;
-		if(m==1){
-			d =  new Date(m+'/1/'+(d.getFullYear()+1));
+		var d = new Date($scope.date);
+		var days = '- ';
+		days = days.repeat(d.getDay());
+		var last = new Date(d);
+		last.setMonth(last.getMonth()+1);
+		last.setDate(last.getDate()-1);
+		last = last.getDate();
+		for(i = 1;i<=last;i++){
+			days=days+i+' ';
 		}
-		d = new Date(m+'/1/'+d.getFullYear());
-		d.setDate(d.getDate()-1);
-		d = d.getDate();
-		$scope.days = d;
-
-		d = $scope.date;
-		$scope.d = d.getDay();
+		d = d.getDay();
+		last+=d;
+		if(last%7!=0){
+			var dash = '- ';
+			dash = dash.repeat((7-last%7));
+			days = days + dash;
+		}
+		days = days.split(' ');
+		days.pop();
+		$scope.days = days;
 	}
 
 	return directive;
