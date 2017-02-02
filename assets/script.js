fd3b6b1b39ded28e35597c543809ddd5c3a2d7ab
someone-1 <iam.nageshbs@gmail.com>
2017-02-02 16:10:13 +0530

    made the code modular

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,6 @@
-app = angular.module("app",[]);
+app = angular.module("app",['calendar']);
 app.controller('main', ['$scope', function($scope){
 
-	$scope.date = new Date('12/1/2017');
+	$scope.date = new Date('9/1/2017');
 
-}])
- 
-
-app.directive('cal', function(){
-
-	var directive = {};
-	directive.restrict = 'E';
-	directive.templateUrl = "/assets/template.html";
-	directive.scope = {
-		date : "=date"
-	};
-
-	directive.controller = function($scope){
-		var d = new Date($scope.date);
-		var days = '- ';
-		days = days.repeat(d.getDay());
-		var last = new Date(d);
-		last.setMonth(last.getMonth()+1);
-		last.setDate(last.getDate()-1);
-		last = last.getDate();
-		for(i = 1;i<=last;i++){
-			days=days+i+' ';
-		}
-		d = d.getDay();
-		last+=d;
-		if(last%7!=0){
-			var dash = '- ';
-			dash = dash.repeat((7-last%7));
-			days = days + dash;
-		}
-		days = days.split(' ');
-		days.pop();
-		$scope.days = days;
-	}
-
-	return directive;
-
-});
+}]);
